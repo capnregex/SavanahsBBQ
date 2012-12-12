7e4c10f3d0283209019005711c9a9d10c780671b
Robert Ferney <capnregex@gmail.com>
2012-12-11 17:29:35 -0700

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SavanahsBBQ::Application.config.secret_token = '007d10b741699882eae20fbd043a42d366acce44cbb5cfc179ac5f61e456df0cefd203e024dd39977c32380a8dd15fb5d27481afb417904ef9f22511c8f0f2fa'
